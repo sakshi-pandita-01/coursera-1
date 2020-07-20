3c84c0f5de8f84ba907d03da5fe0fc37b8b10f89
sakship-bit <66907824+sakship-bit@users.noreply.github.com>
2020-07-20 18:49:27 +0530

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -22,4 +22,4 @@ function speak(name) {
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
- byeSpeaker.byeSpeaker = byeSpeaker;})(window);
+ window.byeSpeaker = byeSpeaker;})(window);
